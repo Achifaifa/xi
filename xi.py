bdeb1c8756752ca633cd63e9ae7fade77928ef6f
Yuri Numerov <achifaifa@meh>
2020-03-09 04:54:22 +0100

    piece movement working

--- xi.py
+++ xi.py
@@ -9,6 +9,9 @@ if debug: print "Starting Xi..."
 size=600
 wsize=(size,size)
 selected=""
+moves=[]
+bestcolour=(250,186,218,8)
+valid_coords=[]
 
 #Pygame stuff
 if debug: print "Initializing pygame...",
@@ -53,9 +56,47 @@ for i in (0,2,4):
     pygame.draw.rect(bg,(128,128,128),((i+j%2)*cellsize,j*cellsize,cellsize,cellsize))
 #Selection square
 sel=pygame.Surface((cellsize,cellsize), pygame.SRCALPHA)
-sel.fill((250,186,218,100))
-sel.set_alpha(100)
+sel.fill(bestcolour)
 
+#functions
+def possible_moves(c):
+  """
+  Draws arrows with the possible moves for a clicked piece
+  """
+
+  piece=board[c[1]][c[0]]
+  piececolour,piecetype=piece.split('_')
+
+  #Check adjacent squares for aons
+  if piecetype=="aon":
+    valid_coords=[[c[0]+1,c[1]],[c[0]-1,c[1]],[c[0],c[1]+1],[c[0],c[1]-1]]
+
+  #Check squares 2 away for khoyors
+  elif piecetype=="khoyor":
+    valid_coords=[[c[0]+2,c[1]],[c[0]-2,c[1]],[c[0],c[1]+2],[c[0],c[1]-2]]
+
+  #Check diagonal squares for skas
+  elif piecetype=="ska":
+    valid_coords=[[c[0]+1,c[1]+1],[c[0]-1,c[1]-1],[c[0]+1,c[1]-1],[c[0]-1,c[1]+1]]
+
+  #Chcek square in front and jokes for sans
+  elif piecetype=="san":
+    pass
+
+  #Remove invalid coords
+  valid_coords=[i for i in valid_coords if all(j>=0 and j<6 for j in i)]
+  valid_coords=[i for i in valid_coords if not board[i[1]][i[0]].startswith(piececolour)]
+  
+  #Draw line
+  pc=lambda x: x*cellsize+cellsize/2 #coordinates of the centre of the cell
+  lsc=(pc(c[0]),pc(c[1])) #line start coordinates
+  lec=[[pc(i[0]),pc(i[1])] for i in valid_coords] #line end coordinates
+  return [valid_coords,[lsc,lec]]
+
+def movepiece(a,b):
+  piece=board[a[1]][a[0]]
+  board[a[1]][a[0]]=""
+  board[b[1]][b[0]]=piece
 
 #Main loop
 if debug: print "  [OK]\nEntering main loop\n---"
@@ -66,7 +107,11 @@ while run:
   for idi,i in enumerate(board):
     for idj,j in enumerate(i):
       position=(cellsize*idj,cellsize*idi)
-      if selected: screen.blit(sel,selected) 
+      if selected: screen.blit(sel,selected)
+      if moves:
+        for z in moves[1]:
+          pygame.draw.line(screen,bestcolour,moves[0],z,10)
+          pygame.draw.circle(screen,bestcolour,z,20)
       if j: screen.blit(eval(j),position)
 
   #Event detection
@@ -77,14 +122,26 @@ while run:
       if keys[pygame.K_ESCAPE]: 
         if debug: print "Escape pressed, quitting"
         run=0
+
     if ev.type==pygame.MOUSEBUTTONDOWN:
       mousepos=pygame.mouse.get_pos()
       coords=[i/cellsize for i in mousepos]
       piece=board[coords[1]][coords[0]]
-      if piece:
+      if coords in valid_coords and selected:
+        if debug: print 'moving '+piece+' to '+str(coords)
+        movepiece(origin,coords)
+        moves=[]
+        selected=""
+        origin=[]
+      elif piece:
         if debug: print "clicked "+piece+" at "+str(coords)
         selected=[coords[0]*cellsize,coords[1]*cellsize]
- 
+        origin=coords
+        valid_coords,moves=possible_moves(coords)
+      else:
+        selected=""
+        moves=[]
+        origin=[]
 
   #Screen and clock update
   pygame.display.flip()
